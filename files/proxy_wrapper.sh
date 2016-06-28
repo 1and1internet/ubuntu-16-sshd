974c608e175e68d9e07270d94097f8219900ac03
Iain Calder <iain.calder@fasthosts.com>
2016-06-28 18:05:10 +0100

    Fix wildcard expansion

--- files/proxy_wrapper.sh
+++ files/proxy_wrapper.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 0 ]] ; then
-        COMMAND=bash
-else
-        COMMAND="$@"
-fi
+# Turn off wildcard expansion
+set -f
 
 USERNAME=$USER
 # Project group is pg_<project-name>
@@ -14,10 +11,16 @@ export OPENSHIFT_CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 export OPENSHIFT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 export OPENSHIFT_URL="https://KUBERNETES_SERVICE_HOST:443"
 
-if [[ -t 0 ]]; then
-        #echo "tty $COMMAND" >> /tmp/proxylog
-	/sshenv -i --tty $PROJECT $USERNAME $COMMAND
+if [[ -t 0 ]]; then        
+        if [ -z "$SSH_ORIGINAL_COMMAND" ]; then
+	        /sshenv -i --tty $PROJECT $USERNAME bash
+        else
+                /sshenv -i --tty $PROJECT $USERNAME bash -c "$SSH_ORIGINAL_COMMAND"
+        fi
 else
-        #echo "notty $COMMAND" >> /tmp/proxylog
-	/sshenv -i $PROJECT $USERNAME $COMMAND      
+        if [ -z "$SSH_ORIGINAL_COMMAND" ]; then
+	        /sshenv -i $PROJECT $USERNAME bash
+        else
+                /sshenv -i $PROJECT $USERNAME bash -c "$SSH_ORIGINAL_COMMAND"
+        fi  
 fi
